b3a2c94ed4dbc3e03bbb2f11e588e56fcbada2ea
Denis Smirnov <desm80@yandex.ru>
2022-09-30 22:19:30 +0300

    файлы для презентации

--- Code/OsPopen.py
+++ Code/OsPopen.py
@@ -1,13 +1,13 @@
 import os
 
 p = os.popen('dir')
-# print(p.__next__())
-# print(p.__next__())
+print(p.__next__().encode('cp1251').decode('cp866'))
+print(p.__next__().encode('cp1251').decode('cp866'))
 # print(next(p))
 
 # Полный протокол итерации
 
-i = iter(p)
-print(next(i))
-print(next(i))
+# i = iter(p)
+# print(next(i))
+# print(next(i))
 
